8634711800643ac055c474065301f9014f1a394e
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-09-11 12:06:41 +0800

    add initial welcome screen

---
+++ src/screens/welcome/styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import {Vcontainer} from '@/component';
+
+export const Container = styled(Vcontainer)`
+  flex: 1;
+`;
+
+export const SafeAreaViewStyled = styled.SafeAreaView``;
