e4875adf862c97bab88349681e32d4087380f9ee
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-09-11 22:01:04 +0800

    add sample list for notes listing

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text, FlatList} from 'react-native';
+import {ListItem} from './component';
+import {NotesList} from './constant';
 
 const Home = () => {
+  const renderItem = ({item}: any) => {
+    return <ListItem title={item.title} body={item.body} date={item.date} />;
+  };
   return (
     <View>
       <Text>home</Text>
+      <FlatList
+        data={NotesList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
